2d0bfad1810e619d33e877985242094aaae5d79d
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-04-04 23:46:27 +0530

    Update chessboard.c

--- codes/chessboard.c
+++ codes/chessboard.c
@@ -11,4 +11,32 @@ int main() {
     u =(n)/8;
     d = 7-u;
     printf("\nAll the trajectories of the Queen from position %d:\n", n);
-   
+     printf("\nLeft:");
+  for (i =l-1;i>=0;i--) {
+      
+        printf("%d ",n-i);
+    }
+    printf("\nRight:");
+    for (i=1;i<= r;i++) {
+      
+        printf("%d ",n+i);
+    }
+    printf("\nUpward:");
+    for (i = u;i>0;i--) {
+      
+        printf("%d ",n-i*8);
+    }
+    printf("\nDownward:");
+    for (i= 1;i<=d;i++) {
+      
+        printf("%d ",n+i*8);
+    }
+    
+    printf("\nUpward-Left:");
+    if(n%8!=1){
+     for (i=1;i<=u&&i<=l;i++){
+        printf(" %d",n-i*9);
+    }
+}
+    return 0;
+}
